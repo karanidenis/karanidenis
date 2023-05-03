ad356d6f508d99363085408b4c1fdeeeccae5da6
karanidenis <d.waweru@alustudent.com>
2023-05-03 12:23:33 +0300

    webpage updates

---
+++ webpage/page.js
@@ -0,0 +1,84 @@
+function showTech() {
+    document.getElementById("tech").style.display = "block";
+    document.getElementById("life").style.display = "none";
+}
+function hideTech() {
+    document.getElementById("tech").style.display = "none";
+    document.getElementById("life").style.display = "block";
+}
+function showLife() {
+    document.getElementById("tech").style.display = "none";
+    document.getElementById("life").style.display = "block";
+}
+function hideLife() {
+    document.getElementById("tech").style.display = "block";
+    document.getElementById("life").style.display = "none";
+}
+function showFront() {
+    document.getElementById("frontlanguages").style.display = "block";
+    document.getElementById("frontimg").style.display = "block";
+    document.getElementById("backimg").style.display = "none";
+    document.getElementById("devimg").style.display = "none";
+    document.getElementById("expimg").style.display = "none";
+    document.getElementById("backlanguages").style.display = "none";
+    document.getElementById("devlanguages").style.display = "none";
+    document.getElementById("fronttools").style.display = "block";
+    document.getElementById("backtools").style.display = "none";
+    document.getElementById("devtools").style.display = "none";
+    document.getElementById("allprojects").style.display = "none";
+    document.getElementById("frontprojects").style.display = "block";
+    document.getElementById("devprojects").style.display = "none";
+    document.getElementById("allexperiences").style.display = "block";
+    document.getElementById("reachme").style.display = "block";
+}
+function showBack() {
+    document.getElementById("frontlanguages").style.display = "none";
+    document.getElementById("backlanguages").style.display = "block";
+    document.getElementById("backimg").style.display = "block";
+    document.getElementById("frontimg").style.display = "none";
+    document.getElementById("devimg").style.display = "none";
+    document.getElementById("expimg").style.display = "none";
+    document.getElementById("devlanguages").style.display = "none";
+    document.getElementById("fronttools").style.display = "none";
+    document.getElementById("backtools").style.display = "block";
+    document.getElementById("devtools").style.display = "none";
+    document.getElementById("allprojects").style.display = "block";
+    document.getElementById("frontprojects").style.display = "none";
+    document.getElementById("devprojects").style.display = "none";
+    document.getElementById("allexperiences").style.display = "none";
+    document.getElementById("reachme").style.display = "none";
+}
+function showDevops() {
+    document.getElementById("devlanguages").style.display = "block";
+    document.getElementById("frontlanguages").style.display = "none";
+    document.getElementById("backlanguages").style.display = "none";
+    document.getElementById("frontimg").style.display = "none";
+    document.getElementById("backimg").style.display = "none";
+    document.getElementById("devimg").style.display = "block";
+    document.getElementById("expimg").style.display = "none";
+    document.getElementById("fronttools").style.display = "none";
+    document.getElementById("backtools").style.display = "none";
+    document.getElementById("devtools").style.display = "block";
+    document.getElementById("allprojects").style.display = "block";
+    document.getElementById("frontprojects").style.display = "none";
+    document.getElementById("devprojects").style.display = "block";
+    document.getElementById("allexperiences").style.display = "none";
+    document.getElementById("reachme").style.display = "none";
+}
+function showExperience() {
+    document.getElementById("frontlanguages").style.display = "none";
+    document.getElementById("backlanguages").style.display = "none";
+    document.getElementById("devlanguages").style.display = "none";
+    document.getElementById("frontimg").style.display = "none";
+    document.getElementById("backimg").style.display = "none";
+    document.getElementById("devimg").style.display = "none";
+    document.getElementById("expimg").style.display = "block";
+    document.getElementById("fronttools").style.display = "none";
+    document.getElementById("backtools").style.display = "none";
+    document.getElementById("devtools").style.display = "none";
+    document.getElementById("allprojects").style.display = "block";
+    document.getElementById("frontprojects").style.display = "none";
+    document.getElementById("devprojects").style.display = "none";
+    document.getElementById("allexperiences").style.display = "block";
+    document.getElementById("reachme").style.display = "block";
+}
